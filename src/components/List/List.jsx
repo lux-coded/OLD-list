2f9392979dd3d5245663543979129eab3d665d30
lux-coded <selakluka@gmail.com>
2021-06-20 19:50:54 +0200

    Cleared unused variables and comments.

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
-import Rating from '@material-ui/lab/Rating';
 
 import ListItem from './ListItem/ListItem';
 import './List.scss';
 
 const List = ({ listData }) => {
   const [ listState, setListState ] = useState(false);
-  // const [ ratingValue, setRatingValue ] = useState(0);
 
   const renderList = () => {
     return listData.map((artist) => {
